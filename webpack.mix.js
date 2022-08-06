f8210473c8a0cfe1ea8b730967bcedd6be00aefa
SystemExecuteLLC <108966041+SystemExecuteLLC@users.noreply.github.com>
2022-08-06 01:44:53 -0700

    Update webpack.mix.js
    Commented out img folder, added background.js and content.js to compilation;

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,12 +12,14 @@ mix.css('src/assets/css/style.css', 'dist/assets/css')
 // compile javascript files
 mix.js('src/app.js', 'dist')
     .js('src/popup.js', 'dist')
+    .js('src/background.js', 'dist')
+    .js('src/content.js', 'dist')
     .vue()
 
 // copy static files,. views and  
 mix.copy('src/views/', 'dist/views/')
     .copy('src/manifest.json', 'dist/')
-    .copy('src/assets/img/', 'dist/assets/img')
+    // .copy('src/assets/img/', 'dist/assets/img') // uncomment when you add images
     .options({
         processCssUrls: false
     })
